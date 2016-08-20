3290a9bd32c1bd243b0454c175bb75d1d8e572dd
Yanze LIU <yanzeliu.france@gmail.com>
2016-08-20 14:54:08 -0700

    Initial Commit

---
+++ custom_view/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  custom_view
+//
+//  Created by yanze on 8/20/16.
+//  Copyright © 2016 yanzeliu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
